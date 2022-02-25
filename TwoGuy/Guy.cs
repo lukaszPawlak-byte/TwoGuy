bdf27d6d5f3008e62a818b7810835d0fb61990e4
lukaszPawlak-byte <lukasz.jacek.pawlak@gmail.com>
2022-02-25 23:17:48 +0100

    Dodaj pliki projektów.

---
+++ TwoGuy/Guy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoGuy
+{
+    class Guy
+    {
+        public string name;
+        public int cash;
+
+        ///<summary>
+        ///Wyświetla w konsoli imie i posiadaną kwotę.
+        ///</summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine("{0} ma {1} zł", name, cash);
+        }
+
+        ///<summary>
+        ///Przekazuje pieniądze i usuwa je z portfela (lub wyświetk
+        ///w konsoli komunikat o braku środków).
+        ///</summary>
+        ///<param name="amount">Przekazana kwota.</param>
+        ///<returns>
+        ///Ilość pieniędzy wyjmowanych z portfela lub 0, jeśli dostępne
+        ///środki są za małe (lub parametr amount jest nieprawidłowy).
+        ///</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("{0} mówi: {1} nie jest właściwą kwotą", name, cash);
+                return 0;
+            }
+            if (amount > cash)
+            {
+                Console.WriteLine("{0} mówi: nie mam wystarczających środków, aby dać Ci {1} zł", name, amount);
+                return 0;
+            }
+            cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Przyjmuje pieniądze i dodaje je do porfela (lub wyświetla
+        /// w konsoli komunikat o nieprawidłowej kwocie).
+        /// </summary>
+        /// <param name="amount">Przyjmowana kwota.</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("{0} mówi: nie przyjmę {1} zł", name, amount);
+            }
+            else
+            {
+                cash += amount;
+            }
+
+        }
+    }
+}
---
+++ TwoGuy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TwoGuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Guy jacek = new Guy() {cash = 50, name = "Jacek" };
+            Guy bartek = new Guy() {cash = 100, name = "Bartek"};
+
+            while (true)
+            {
+                jacek.WriteMyInfo();
+                bartek.WriteMyInfo();
+
+                Console.Write("Podaj kwotę: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.WriteLine("Pieniądze ma przekazać ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Jacek")
+                    {
+                        jacek.GiveCash(amount);
+                        bartek.ReceiveCash(amount);
+                        Console.WriteLine("osobie {0} ma {1} zł", jacek.name, jacek.cash);
+                        Console.WriteLine("osobie {0} ma {1} zł", bartek.name, bartek.cash);
+                    }
+                    else if (whichGuy == "Bartek")
+                    {
+                        bartek.GiveCash(amount);
+                        jacek.ReceiveCash(amount);
+                        Console.WriteLine("osobie {0} ma {1} zł", jacek.name, jacek.cash);
+                        Console.WriteLine("osobie {0} ma {1} zł", bartek.name, bartek.cash);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wpisz kwotę (lub pusty wiersz aby zakończyć)");
+                    }
+                }
+            }
+            
+        }
+    }
+}
